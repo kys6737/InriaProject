d06fd8a09b4663e0a39ebe7738a90277c2ec6bdc
kys6737 <gimyeongsu6737@gmail.com>
2024-05-09 23:18:13 +0900

    임시저장

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
-import ImageData from './image.json';
+
+import bellingham from './data/bellingham.json';
+import innsbruck from './data/innsbruck.json';
+//파일 이름 맞춰서 계속 import.
 
 function App() {
+  /* Variables */
+  // State
+  const [placeState, setPlaceState] = useState('index');
+  function handlePlaceState(name) {
+    return function() {
+      setPlaceState(name);
+    };
+  }
+  // List
+  const place = [
+    {key: 'bellingham', value: bellingham}, 
+    {key: 'innsbruck', value: innsbruck}
+  ]
+
+  /* Function */
+  function Nav() {
+    return (
+      <nav>
+        {place.map(element => (
+          <button onClick={handlePlaceState(element.value)}>{element.key}</button>
+        ))}
+      </nav>
+    );
+  }
+
   return (
-    <div className="grid-container"> 
-      {Object.entries(ImageData).map(([key, path]) => (
-        <img key={key} className="grid-item" src={require(`${path}`)} alt={key} />
-      ))}
-    </div>
-  );
+    <>
+      <Nav />
+      {placeState !== 'index' && 
+        (<div className="grid-container"> 
+          {Object.entries(placeState).map(([key, path]) => (
+          <img key={key}  alt={key} className="grid-item" src={require(`${path}`)} />
+        ))}</div>)}
+    </>
+  )
 }
 
 export default App;
